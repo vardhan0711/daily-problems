5d7112b095e5ef50ffb206c728646b2d1766d1a5
yashvardhan singh <150661711+vardhan0711@users.noreply.github.com>
2025-06-18 19:42:29 +0530

    Time: 43 ms (84.69%), Space: 110.6 MB (99.04%) - LeetHub

--- 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/3241-divide-array-into-arrays-with-max-difference.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
-        vector<vector<int>> res(nums.size()/3,vector<int>(3));
-        sort(nums.begin(),nums.end());
-        int ind=0;
-        for(int i=0;i<nums.size();i+=3){
-            if(i+2<nums.size()&&nums[i+2]-nums[i]<=k){
-                res[ind]={nums[i],nums[i+1],nums[i+2]};
-                ind++;
-            }else{
-                return vector<vector<int>>();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        res.reserve(nums.size() / 3);  // avoid repeated reallocations
+
+        for (int i = 0; i < nums.size(); i += 3) {
+            // check we have at least 3 elements left
+            if (i + 2 >= nums.size() || nums[i + 2] - nums[i] > k) {
+                return {};  // can't form valid group
             }
+            res.push_back({nums[i], nums[i + 1], nums[i + 2]});
         }
         return res;
     }
-
 };
