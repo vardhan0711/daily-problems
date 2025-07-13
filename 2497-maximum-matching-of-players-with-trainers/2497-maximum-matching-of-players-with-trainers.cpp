8225fa232039487f9fdc83a2a821e30941b49eb4
yashvardhan singh <150661711+vardhan0711@users.noreply.github.com>
2025-07-13 10:02:47 +0530

    Time: 23 ms (95.97%), Space: 80.1 MB (85.54%) - LeetHub

--- 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int matchPlayersAndTrainers(std::vector<int>& players, std::vector<int>& trainers) {
-        std::sort(players.begin(), players.end());
-        std::sort(trainers.begin(), trainers.end());
-
-        int count = 0;
-        int i = 0, j = 0; // pointers for players and trainers
-
-        while (i < players.size() && j < trainers.size()) {
-            if (players[i] <= trainers[j]) {
-                // Match found
-                count++;
-                i++;
+    static int matchPlayersAndTrainers(vector<int>& p, vector<int>& t) 
+    {
+        const int n=p.size(), m=t.size();
+        sort(p.begin(), p.end());
+        sort(t.begin(), t.end());
+        int match=0;
+        for (int i=0, j=0; i<n && j<m; i++){
+            const int pi=p[i];
+            while(j<m && t[j]<pi) j++;
+            if (j<m) {
                 j++;
-            } else {
-                // Try next trainer
-                j++;
+                match++;
             }
-        }
-        return count;
+        } 
+        return match;
     }
 };
