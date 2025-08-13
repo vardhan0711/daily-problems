6d19b601e13436e6f1b4e28abefb367f3b81cfa5
Yashvardhan Singh <150661711+vardhan0711@users.noreply.github.com>
2025-08-13 11:01:34 +0530

    Time: 3 ms (60.14%), Space: 9 MB (14.61%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,8 +1,10 @@
+//using log operator 
+
 class Solution {
 public:
     bool isPowerOfThree(int n) {
         if(n<=0)return false;
-        int max=pow(3,19);
-        return (max%n==0);
+        double x=log10(n)/log10(3);
+        return x==(int)x;
     }
 };
