046e3fef235184b220491e76906ebcf876a94682
Yashvardhan Singh <150661711+vardhan0711@users.noreply.github.com>
2025-08-13 10:49:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,13 +1,6 @@
-//no idea of bit manipulation solution 
-
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n==0)return false;
-        if(n==1)return true;
-        if(n>1)return n%3==0&&isPowerOfThree(n/3);
-        else{
-            return false;
-        }
+        
     }
 };
