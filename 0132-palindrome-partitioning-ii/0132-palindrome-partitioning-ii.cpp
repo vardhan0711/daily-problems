6e39cad8b66402894a0fe15f7153d24d20b3bad9
Yashvardhan Singh <150661711+vardhan0711@users.noreply.github.com>
2025-09-01 21:42:02 +0530

    Time: 43 ms (87.72%), Space: 12 MB (42.39%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    int t[2001][2001];
-    bool pal[2001][2001];  // store palindrome info
-
-    void precomputePalindrome(const string &s) {
-        int n = s.size();
-        for (int i = 0; i < n; i++) pal[i][i] = true; // single chars
-        for (int i = 0; i + 1 < n; i++) pal[i][i + 1] = (s[i] == s[i + 1]); // length 2
-
-        for (int len = 3; len <= n; len++) {
-            for (int i = 0; i + len - 1 < n; i++) {
-                int j = i + len - 1;
-                pal[i][j] = (s[i] == s[j]) && pal[i + 1][j - 1];
-            }
-        }
-    }
-
-    int solve(const string &s, int i, int j) {
-        if (pal[i][j]) return 0;  // already palindrome
-        if (t[i][j] != -1) return t[i][j];
-
-        int res = INT_MAX;
-        for (int k = i; k < j; k++) {
-            if (pal[i][k]) { // only cut if left side is palindrome
-                res = min(res, 1 + solve(s, k + 1, j));
-            }
-        }
-        return t[i][j] = res;
-    }
-
     int minCut(string s) {
         int n = s.size();
-        memset(t, -1, sizeof(t));
-        memset(pal, 0, sizeof(pal));
-        precomputePalindrome(s);
-        return solve(s, 0, n - 1);
+        vector<vector<bool>> isPal(n, vector<bool>(n, false));
+        
+        // Precompute palindrome substrings
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                if (s[i] == s[j] && (len <= 2 || isPal[i + 1][j - 1])) {
+                    isPal[i][j] = true;
+                }
+            }
+        }
+
+        // dp[i] = min cuts for substring [0..i]
+        vector<int> dp(n, INT_MAX);
+        for (int i = 0; i < n; i++) {
+            if (isPal[0][i]) {
+                dp[i] = 0; // whole substring [0..i] is a palindrome
+            } else {
+                for (int j = 0; j < i; j++) {
+                    if (isPal[j + 1][i]) {
+                        dp[i] = min(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+        }
+        
+        return dp[n - 1];
     }
 };
