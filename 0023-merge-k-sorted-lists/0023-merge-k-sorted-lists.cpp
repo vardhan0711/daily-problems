1f34bc393d5bad3f9b31c538a295d1c8453d405a
Yashvardhan Singh <150661711+vardhan0711@users.noreply.github.com>
2025-10-13 22:26:02 +0530

    Time: 4 ms (47.72%), Space: 18.4 MB (65.88%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,12 +1,11 @@
-
+//overall TC->O(N * log k) and SC->O(N)
 class Solution {
 public:
-    //this function will take care of the merginf of two lists at a time 
-    
+    //this function will take care of the merginf of two lists at a time
     ListNode* mergeTwoList(ListNode* l1,ListNode* l2){
         if(!l1)return l2;
         if(!l2)return l1;
-        if(l1->val<=l2->val){
+        if(l1->val<=l2->val){//if l1 is smaller then merge the rest of l1 with l2
             l1->next=mergeTwoList(l1->next,l2);
             return l1;
         }else{
@@ -14,6 +13,8 @@ public:
             return l2;
         }
     }
+    //this will divide the list into two parts and then send it for merging to the merger function
+    //TC->O(N * log k) and SC->O(N)
     ListNode* divideAndMerge(int start,int end,vector<ListNode*>& lists){
         if(start>end)return NULL;
         if(start==end)return lists[start];
